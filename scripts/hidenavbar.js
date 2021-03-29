8be9433090ae39f35e8e248b7dd0ccff9dad9b36
danielulloa <danielulloa1994@gmail.com>
2021-03-29 17:16:54 -0300

    main website refactored
    Co-authored-by: igr91 <igreinaudo@gmail.com>
    
    29/03/21 - igr91
    ===================================
    - arreglados errores de maquetado según validador html de w3c
    - agregado texto alt a cada imagen
    - achicado tamaño de logos en navbar en vista y rehechas reglas <1080p
    - rehechas reglas CSS de <article> y relacionados, estilos levemente distintos
    - agregadas reglas CSS para botones y contenido nuevo
    - replicados cambios en la versión en inglés del sitio
    - navbar se retrae al scrollear para abajo en sitio mobile
    - movido el sitio en inglés al root junto al original para facilitar el cambio por el momento
    
    28/03/21 - danielulloa
    ===================================
    - cambia contenido carousel
    - agrega actividades con nuevo template
    - agrega banners de publicidad
    - agrega icono de lenguaje en navbar
    - agrega version en inglés
    
    28/03/21 - igr91
    ===================================
    - actualizado FontAwesome a su última versión
    - actualizadas referencias de iconos FontAwesome
    - icono ieee como fuente custom para el navbar
    - color del fondo restaurado al original previo al rediseño
    - agregados templates de <article> con imagen a la derecha o izquierda
    - retoques responsive
    - en Android ahora el statusbar toma el color del navbar del sitio
    
    28/03/21 - danielulloa
    ===================================
    - cambia contenido
    - agrega codigo de etica
    - intento de icono svg con fontawesome
    
    28/03/21 - igr91
    ===================================
    - arreglado y ajustado diseño responsive, probado en celular Android 1080p y
    con las vistas de navegador para ~720p
    - arreglada la sección de las fotos de los officers
    - los correos de los officers ahora permiten enviarles correo con hacer clic
    - arreglados links en general
    - agregado scroll suave
    
    27/03/21 - igr91
    ===================================
    - maquetado nuevo, mantenible y extensible
    - layout de archivos lógico (carpetas para cada cosa)
    - CSS nuevo, sass fuente -> css
    - diseño responsive para cualquier tamaño de pantalla, basado en flexbox
    
    - fotos optimizadas en tamaño sin sacrificar demasiada calidad
    - fotos redimensionadas para que se lleven bien con el maquetado
    
    - carousel de fotos simplificado
    - navegación simplificada
    
    - traté de adherir lo más posible a las guías de la IEEE (tipografía, colores)

---
+++ scripts/hidenavbar.js
@@ -0,0 +1,12 @@
+if (screen.width <= 1080) {
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-116px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
